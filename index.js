14dbe2adb2cb51f2c9b357f6f9edfa956b9e778c
Luiz Fábio Santana <f4biosantana@gmail.com>
2024-02-23 11:10:35 -0300

    terminado a parte que códifica

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // criptografar
 // descriptografar
-let textEncriptar = '';
-let textDescriptar = '';
+let inputTexto = document.getElementById("textInput");
+let output = document.getElementById("output");
+let textOutput = document.getElementsByClassName("text-output")[0];
+let textCodified = document.getElementsByClassName("text-codified")[0];
+
 function cifraDeCesar(texto, chave, criptografar) {
     // a função recebe esse dados substituindo o valor de cada caractere da string; por o resultado;
     // do callback no caso é o (criptografar)
@@ -15,27 +18,45 @@ function cifraDeCesar(texto, chave, criptografar) {
     });
 }
 
-function lidarComCampoVazio() {
-    let classes = ["svgImg", "outputTitle", "outputText"];
-    for (let i = 0; i < classes.length; i++) {
-        let element = document.getElementsByClassName(classes[i])[0];
-        element.classList.add('active');
+function lidarComCampoVazio(value) {
+
+    if (value) {
+        if (!textCodified) {
+            let classes = ["svgImg", "outputTitle", "outputText"];
+            for (let i = 0; i < classes.length; i++) {
+                let element = document.getElementsByClassName(classes[i])[0];
+                element.classList.add('active');
+            }
+        } else {
+            lidarComCampoVazio();
+        }
+    } else {
+        let classes = ["svgImg", "outputTitle", "outputText"];
+        for (let i = 0; i < classes.length; i++) {
+            let element = document.getElementsByClassName(classes[i])[0];
+            element.classList.remove('active');
+        }
     }
+}
+function lidarComTexto(text) {
+
 }
 
 function criptografar() {
-    let inputTexto = document.getElementById("textInput");
     if (!inputTexto.value) {
+        lidarComCampoVazio(true);
+    } else {
+        textCodified.innerHTML = cifraDeCesar(inputTexto.value, 3, true);
+        textOutput.classList.add("active");
         lidarComCampoVazio()
+        inputTexto.value = "";
     }
-
-    // textEncriptar = inputTexto.value;
-    // if (inputTexto.value) {
-    //     textEncriptar = cifraDeCesar(textEncriptar, 3, true);
-    // }
-    // console.log(textEncriptar);
 }
 
+// let iconElement = document.createElement("img")
+// iconElement.src = `./assets/images/${card.icon}.png`;
+// iconElement.classList.add(ICON)
+// cardElementFace.appendChild(iconElement);
 
 // let textoOriginal = "Olá tudo bem?";
 // let textoCifrado = cifraDeCesar(textoOriginal, 3, true);
